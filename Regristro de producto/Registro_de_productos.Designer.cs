9384863f82f7659909f313a0639f96a4e8545eba
s23120189 <s23120189@alumnos.itsur.edu.mx>
2025-11-26 22:32:00 -0600

    ese es el de registro

---
+++ Regristro de producto/Registro_de_productos.Designer.cs
@@ -0,0 +1,197 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: compu1
+ * Date: 24/11/2025
+ * Time: 08:30 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace proyectoInventario
+{
+    partial class RegistrarProducto
+    {
+        /// <summary>
+        /// Designer variable used to keep track of non-visual components.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Disposes resources used by the form.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// This method is required for Windows Forms designer support.
+        /// Do not change the method contents inside the source code editor. The Forms designer might
+        /// not be able to load this method if it was changed manually.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.lblTitleMain = new System.Windows.Forms.Label();
+            this.txtIDProducto = new System.Windows.Forms.TextBox();
+            this.txtNombreProducto = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtStockInicial = new System.Windows.Forms.TextBox();
+            this.btnAgregarInv = new System.Windows.Forms.Button();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitleMain
+            // 
+            this.lblTitleMain.AutoSize = true;
+            this.lblTitleMain.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblTitleMain.ForeColor = System.Drawing.Color.White;
+            this.lblTitleMain.Location = new System.Drawing.Point(128, 53);
+            this.lblTitleMain.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitleMain.Name = "lblTitleMain";
+            this.lblTitleMain.Size = new System.Drawing.Size(230, 32);
+            this.lblTitleMain.TabIndex = 3;
+            this.lblTitleMain.Text = "Registrar Producto";
+            // 
+            // txtIDProducto
+            // 
+            this.txtIDProducto.Location = new System.Drawing.Point(65, 112);
+            this.txtIDProducto.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtIDProducto.Name = "txtIDProducto";
+            this.txtIDProducto.Size = new System.Drawing.Size(372, 22);
+            this.txtIDProducto.TabIndex = 4;
+            // 
+            // txtNombreProducto
+            // 
+            this.txtNombreProducto.Location = new System.Drawing.Point(65, 159);
+            this.txtNombreProducto.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNombreProducto.Name = "txtNombreProducto";
+            this.txtNombreProducto.Size = new System.Drawing.Size(372, 22);
+            this.txtNombreProducto.TabIndex = 5;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(65, 207);
+            this.txtPrecio.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(372, 22);
+            this.txtPrecio.TabIndex = 6;
+            // 
+            // txtStockInicial
+            // 
+            this.txtStockInicial.Location = new System.Drawing.Point(65, 251);
+            this.txtStockInicial.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtStockInicial.Name = "txtStockInicial";
+            this.txtStockInicial.Size = new System.Drawing.Size(372, 22);
+            this.txtStockInicial.TabIndex = 7;
+            // 
+            // btnAgregarInv
+            // 
+            this.btnAgregarInv.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(75)))), ((int)(((byte)(255)))));
+            this.btnAgregarInv.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAgregarInv.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAgregarInv.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregarInv.ForeColor = System.Drawing.Color.White;
+            this.btnAgregarInv.Location = new System.Drawing.Point(147, 298);
+            this.btnAgregarInv.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAgregarInv.Name = "btnAgregarInv";
+            this.btnAgregarInv.Size = new System.Drawing.Size(207, 47);
+            this.btnAgregarInv.TabIndex = 8;
+            this.btnAgregarInv.Text = "Agregar al Inventario";
+            this.btnAgregarInv.UseVisualStyleBackColor = false;
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(50)))), ((int)(((byte)(86)))));
+            this.btnVolver.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnVolver.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVolver.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVolver.ForeColor = System.Drawing.Color.White;
+            this.btnVolver.Location = new System.Drawing.Point(1008, 25);
+            this.btnVolver.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(100, 47);
+            this.btnVolver.TabIndex = 0;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = false;
+            this.btnVolver.Click += new System.EventHandler(this.BtnVolver_Click);
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(35)))));
+            this.dgvProductos.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvProductos.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dgvProductos.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.MenuBar;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe MDL2 Assets", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvProductos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Location = new System.Drawing.Point(43, 383);
+            this.dgvProductos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dgvProductos.RowHeadersWidth = 51;
+            this.dgvProductos.Size = new System.Drawing.Size(1065, 251);
+            this.dgvProductos.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(65, 358);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 23);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Productos Registrados";
+            // 
+            // RegistrarProducto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(13)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(1139, 649);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvProductos);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.btnAgregarInv);
+            this.Controls.Add(this.txtStockInicial);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtNombreProducto);
+            this.Controls.Add(this.txtIDProducto);
+            this.Controls.Add(this.lblTitleMain);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "RegistrarProducto";
+            this.Text = "Registrar Producto";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvProductos;
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Button btnAgregarInv;
+        private System.Windows.Forms.TextBox txtStockInicial;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtNombreProducto;
+        private System.Windows.Forms.TextBox txtIDProducto;
+        private System.Windows.Forms.Label lblTitleMain;
+    }
+}
---
+++ Regristro de producto/Registro_de_productos.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace proyectoInventario
+{
+    public partial class RegistrarProducto : Form
+    {
+        string conexion = "Server=localhost;Database=VENTAS;Uid=root;Pwd=;";
+
+        public RegistrarProducto()
+        {
+            InitializeComponent();
+            CargarProductos();
+        }
+
+        // ==========================================
+        //           OBTENER PRODUCTOS
+        // ==========================================
+        public List<Producto> ObtenerProductos()
+        {
+            List<Producto> lista = new List<Producto>();
+
+            using (MySqlConnection conn = new MySqlConnection(conexion))
+            {
+                conn.Open();
+                string query = "SELECT CLAVE, NOMBRE, PRECIO, STOCK FROM Producto";
+
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                MySqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    lista.Add(new Producto
+                    {
+                        Clave = dr.GetString("CLAVE"),
+                        Nombre = dr.GetString("NOMBRE"),
+                        Precio = dr.GetDecimal("PRECIO"),
+                        Stock = dr.GetInt32("STOCK")
+                    });
+                }
+            }
+
+            return lista;
+        }
+
+        // ==========================================
+        //           CARGAR DATOS AL GRID
+        // ==========================================
+        public void CargarProductos()
+        {
+            dgvProductos.DataSource = ObtenerProductos();
+        }
+
+        // ==========================================
+        //           BOTÓN VOLVER
+        // ==========================================
+        private void BtnVolver_Click(object sender, EventArgs e)
+        {
+            Main ventanaPrincipal = new Main();  // Nombre correcto tu ventana inicial
+            ventanaPrincipal.Show();
+            this.Close();
+        }
+
+        // ==========================================
+        //         AGREGAR PRODUCTO NUEVO
+        // ==========================================
+        private void btnAgregarInv_Click(object sender, EventArgs e)
+        {
+            string id = txtIDProducto.Text;
+            string nombre = txtNombreProducto.Text;
+            decimal precio;
+            int stock;
+
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                MessageBox.Show("Ingrese un ID de producto.");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(nombre))
+            {
+                MessageBox.Show("Ingrese un nombre de producto.");
+                return;
+            }
+
+            if (!decimal.TryParse(txtPrecio.Text, out precio))
+            {
+                MessageBox.Show("Ingrese un precio válido.");
+                return;
+            }
+
+            if (!int.TryParse(txtStockInicial.Text, out stock))
+            {
+                MessageBox.Show("Ingrese un stock inicial válido.");
+                return;
+            }
+
+            // Revisar si ya existe
+            var productos = ObtenerProductos();
+            if (productos.Any(p => p.Clave == id))
+            {
+                MessageBox.Show("Este ID ya existe. Use otro.");
+                return;
+            }
+
+            // Insertar en BD
+            using (MySqlConnection conn = new MySqlConnection(conexion))
+            {
+                conn.Open();
+
+                string InsertQuery = @"INSERT INTO Producto (CLAVE, NOMBRE, PRECIO, STOCK)
+                                       VALUES (@id, @nombre, @precio, @stock)";
+
+                MySqlCommand cmd = new MySqlCommand(InsertQuery, conn);
+
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@nombre", nombre);
+                cmd.Parameters.AddWithValue("@precio", precio);
+                cmd.Parameters.AddWithValue("@stock", stock);
+
+                cmd.ExecuteNonQuery();
+            }
+
+            MessageBox.Show("Producto registrado con éxito.");
+
+            // Limpiar campos
+            txtIDProducto.Clear();
+            txtNombreProducto.Clear();
+            txtPrecio.Clear();
+            txtStockInicial.Clear();
+
+            CargarProductos();
+        }
+    }
+
+    // ==========================================
+    //              CLASE PRODUCTO
+    // ==========================================
+    public class Producto
+    {
+        public string Clave { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+    }
+}
